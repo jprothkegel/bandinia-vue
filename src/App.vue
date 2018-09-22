43ecb15e43d04fc9246cc98fcfdd09c47db9e6b5
jprothkegel <juan.rothkegel.12@sansano.usm.cl>
2018-09-22 13:19:51 -0300

    Fixed: Navigation Drawer

--- src/App.vue
+++ src/App.vue
@@ -5,66 +5,53 @@
       v-model="drawer"
       fixed
       app
+      temporary
     >
       <v-list dense>
-        <template v-for="item in items">
-          <v-layout
-            v-if="item.heading"
-            :key="item.heading"
-            row
-            align-center
-          >
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-            <v-flex xs6 class="text-xs-center">
-              <a href="#!" class="body-2 black--text">EDIT</a>
-            </v-flex>
-          </v-layout>
-          <v-list-group
-            v-else-if="item.children"
-            v-model="item.model"
-            :key="item.text"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
-          >
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile
-              v-for="(child, i) in item.children"
-              :key="i"
-              @click=""
-            >
-              <v-list-tile-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ child.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-group>
-          <v-list-tile v-else :key="item.text" @click="">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
+        <v-subheader>Menú</v-subheader>
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-btn depressed small flat round>
+              Quienes Somos
+            </v-btn>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile>
+          <v-list-tile-content>
+            <v-btn depressed small flat round>
+              Historia
+            </v-btn>
+          </v-list-tile-content>
+        </v-list-tile>
+        
+      <v-list-tile>
+          <v-list-tile-content>
+            <v-btn depressed small flat round>
+              Nuestro Hogar
+            </v-btn>
+          </v-list-tile-content>
+        </v-list-tile>
+
+      <v-list-tile>
+          <v-list-tile-content>
+            <v-btn depressed small flat round>
+              Activos
+            </v-btn>
+          </v-list-tile-content>
+        </v-list-tile>
+
+      <v-list-tile>
+          <v-list-tile-content>
+            <v-btn depressed small flat round>
+              Galeria
+            </v-btn>
+          </v-list-tile-content>
+        </v-list-tile>
+
       </v-list>
     </v-navigation-drawer>
+    
     <v-toolbar
       :clipped-left="$vuetify.breakpoint.lgAndUp"
       color="green darken-4"
@@ -73,7 +60,7 @@
       fixed
     >
       <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
         <v-btn icon large>
         <v-avatar size="32px" tile>
           <img
@@ -85,21 +72,23 @@
       </v-toolbar-title>
       
       <v-spacer></v-spacer>
-      <v-btn depressed small flat round>
-        Quienes Somos
-      </v-btn>
-      <v-btn depressed small flat round>
-        Historia
-      </v-btn>
-      <v-btn depressed small flat round>
-        Nuestro Hogar
-      </v-btn>
-      <v-btn depressed small flat round>
-        Activos
-      </v-btn>
-      <v-btn depressed small flat round>
-        Galeria
-      </v-btn>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn depressed small flat round>
+          Quienes Somos
+        </v-btn>
+        <v-btn depressed small flat round>
+          Historia
+        </v-btn>
+        <v-btn depressed small flat round>
+          Nuestro Hogar
+        </v-btn>
+        <v-btn depressed small flat round>
+          Activos
+        </v-btn>
+        <v-btn depressed small flat round>
+          Galeria
+        </v-btn>
+      </v-toolbar-items>
 
     </v-toolbar>
     <v-content>
@@ -113,7 +102,6 @@
     height="auto"
     color="black"
     inset
-    style='z-index:100000000;'
   >
     <v-layout justify-center row wrap>
       <v-flex xs12 md6>
